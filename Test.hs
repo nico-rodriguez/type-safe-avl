562297149e1fe82dcebe08d3772ed9f5f6f23fab
nico-rodriguez <nico-rodriguez@users.noreply.github.com>
2019-05-05 22:40:59 -0300

    Updated tests.

--- Test.hs
+++ Test.hs
@@ -4,33 +4,33 @@ module Test where
 
 import BST
 
-sbn0 :: SingletonBNat ('Val 'Z)
-sbn0 = Natty Zy
+sbn0 :: Natty 'Z
+sbn0 = Zy
 
-sbn1 :: SingletonBNat ('Val ('S 'Z))
-sbn1 = Natty (Sy Zy)
+sbn1 :: Natty ('S 'Z)
+sbn1 = Sy Zy
 
-sbn2 :: SingletonBNat ('Val ('S ('S 'Z)))
-sbn2 = Natty (Sy (Sy Zy))
+sbn2 :: Natty ('S ('S 'Z))
+sbn2 = Sy (Sy Zy)
 
-sbn3 :: SingletonBNat ('Val ('S ('S ('S 'Z))))
-sbn3 = Natty (Sy (Sy (Sy Zy)))
+sbn3 :: Natty ('S ('S ('S 'Z)))
+sbn3 = Sy (Sy (Sy Zy))
 
-sbn4 :: SingletonBNat ('Val ('S ('S ('S ('S 'Z)))))
-sbn4 = Natty (Sy (Sy (Sy (Sy Zy))))
+sbn4 :: Natty ('S ('S ('S ('S 'Z))))
+sbn4 = Sy (Sy (Sy (Sy Zy)))
 
-sbn5 :: SingletonBNat ('Val ('S ('S ('S ('S ('S 'Z))))))
-sbn5 = Natty (Sy (Sy (Sy (Sy (Sy Zy)))))
+sbn5 :: Natty ('S ('S ('S ('S ('S 'Z)))))
+sbn5 = Sy (Sy (Sy (Sy (Sy Zy))))
 
-sbn6 :: SingletonBNat ('Val ('S ('S ('S ('S ('S ('S 'Z)))))))
-sbn6 = Natty (Sy (Sy (Sy (Sy (Sy (Sy Zy))))))
+sbn6 :: Natty ('S ('S ('S ('S ('S ('S 'Z))))))
+sbn6 = Sy (Sy (Sy (Sy (Sy (Sy Zy)))))
 
-e :: BST 'MinusInf 'PlusInf
-e = EmptyBST MinusInfy PlusInfy
+e :: BST 'Bot 'Top
+e = EmptyBST
 
-t3 :: BST 'MinusInf 'PlusInf
+t3 :: BST 'Bot 'Top
 -- t3 = RootBST (EmptyBST MinusInfy sbn3) sbn3 (EmptyBST sbn3 PlusInfy)
 t3 = insert sbn3 e
 
-t2 :: BST 'MinusInf 'PlusInf
-t2 = RootBST (EmptyBST MinusInfy sbn2) sbn2 (EmptyBST sbn2 PlusInfy)
+t2 :: BST 'Bot 'Top
+t2 = RootBST EmptyBST sbn2 EmptyBST
