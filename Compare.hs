46c058ebceb0fa6ab4c893cff80d9586c00cc486
Nicolás Rodríguez <18080513+nico-rodriguez@users.noreply.github.com>
2019-07-26 17:04:05 -0300

    Add Compare.hs.

---
+++ Compare.hs
@@ -0,0 +1,20 @@
+{-# LANGUAGE DataKinds #-}
+{-# LANGUAGE GADTs #-}
+{-# LANGUAGE TypeFamilies #-}
+{-# LANGUAGE TypeOperators #-}
+{-# LANGUAGE PolyKinds #-}
+
+module Compare where
+
+import Data.Proxy
+import Data.Type.Equality
+
+type family Compare (a :: k) (b :: k) :: Ordering
+
+data OWOTO :: a -> b -> * where
+  LE :: (Compare x y ~ 'LT) => OWOTO x y
+  EE :: (Compare x x ~ 'EQ) => OWOTO x x
+  GE :: (Compare x y ~ 'GT) => OWOTO x y
+
+owoto :: s1 a -> s2 b -> OWOTO a b
+owoto = undefined
