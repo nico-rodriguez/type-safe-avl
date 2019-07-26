2be5b7da9993767ff974537d250c835b3a36e74e
Nicolás Rodríguez <18080513+nico-rodriguez@users.noreply.github.com>
2019-07-26 17:05:58 -0300

    Define Str singleton for strings. Add owoto for Str.

--- Str.hs
+++ Str.hs
@@ -1,9 +1,7 @@
 {-# LANGUAGE DataKinds #-}
-
+{-# LANGUAGE GADTs #-}
 {-# LANGUAGE TypeFamilies #-}
-
 {-# LANGUAGE TypeOperators #-}
-
 {-# LANGUAGE UndecidableInstances #-}
 
 module Str where
@@ -12,7 +10,11 @@ import Chr
 import Data.Type.Bool
 import Data.Type.Equality
 
-newtype Str = String [Chr]
+-- newtype Str = String [Chr]
+
+data Str :: [Chr] -> * where
+  EmptyStr :: Str '[]
+  SStr     :: SChar c -> Str cs -> Str (c:cs)
 
 type family CompareStr (a :: [Chr]) (b :: [Chr]) :: Ordering where
   CompareStr '[]    '[]     = 'EQ
@@ -26,3 +28,17 @@ type family CompareStr (a :: [Chr]) (b :: [Chr]) :: Ordering where
         'GT
       )
     )
+
+data OWOTO :: [Chr] -> [Chr] -> * where
+  LE :: (CompareStr x y ~ 'LT) => OWOTO x y
+  EE :: (CompareStr x x ~ 'EQ) => OWOTO x x
+  GE :: (CompareStr x y ~ 'GT) => OWOTO x y
+
+owoto :: Str m -> Str n -> OWOTO m n
+owoto EmptyStr    EmptyStr    = EE
+owoto EmptyStr    (SStr d ds) = LE
+owoto (SStr c cs) EmptyStr    = GE
+owoto (SStr c cs) (SStr d ds) = case owoto c d of
+  LE -> LE
+  GE -> GE
+  EE -> EE
