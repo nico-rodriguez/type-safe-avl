cc62642809e37b01a1f7b269b41271fb770a801f
Nicolás Rodríguez <18080513+nico-rodriguez@users.noreply.github.com>
2019-07-31 22:40:55 -0300

    Finished BST incremental version.

--- ITreeNatIncremental/ITreeNat.hs
+++ ITreeNatIncremental/ITreeNat.hs
@@ -25,21 +25,6 @@ data ITree :: Tree -> * where
   EmptyITree :: ITree 'EmptyTree
   ForkITree  :: ITree l -> Natty n -> ITree r -> ITree ('ForkTree l n r)
 
-class IsBST (t :: Tree) where
-instance IsBST 'EmptyTree where
-instance (IsBST l, IsBST r, LtN l n, GtN r n) => IsBST ('ForkTree l n r) where
-
-class LtN (t :: Tree) (x :: Nat) where
-instance LtN 'EmptyTree x where
-instance (LtN l x, Compare n x ~ 'LT, LtN r x) => LtN ('ForkTree l n r) x
-
-class GtN (t :: Tree) (x :: Nat) where
-instance GtN 'EmptyTree x where
-instance (GtN l x, Compare n x ~ 'GT, GtN r x) => GtN ('ForkTree l n r) x
-
-data BST :: Tree -> * where
-  BST :: (IsBST t) => ITree t -> BST t
-
 instance Show (ITree t) where
   show EmptyITree         = "E"
   show (ForkITree l n r)  = "F " ++ go l ++ " " ++ show n ++ " " ++ go r
@@ -47,6 +32,63 @@ instance Show (ITree t) where
       go :: ITree t' -> String
       go EmptyITree         = "E"
       go (ForkITree l' n' r')  = "(" ++ go l' ++ " " ++ show n' ++ " " ++ go r' ++ ")"
+
+class IsBST (t :: Tree) where
+instance IsBST 'EmptyTree where
+instance (IsBST l, IsBST r, LtN l n, GtN r n) =>
+  IsBST ('ForkTree l n r) where
+
+class LtN (t :: Tree) (x :: Nat) where
+instance LtN 'EmptyTree x where
+instance (LtN l x, Compare n x ~ 'LT, LtN r x) =>
+  LtN ('ForkTree l n r) x
+
+class GtN (t :: Tree) (x :: Nat) where
+instance GtN 'EmptyTree x where
+instance (GtN l x, Compare n x ~ 'GT, GtN r x) =>
+  GtN ('ForkTree l n r) x
+
+data BST :: Tree -> * where
+  BST :: (IsBST t) => ITree t -> BST t
+
+data ProofBST :: Tree -> * where
+  PE :: ProofBST 'EmptyTree
+  PF :: (IsBST l, IsBST r, LtN l n, GtN r n, IsBST ('ForkTree l n r)) =>
+    ProofBST l -> Natty n -> ProofBST r -> ProofBST ('ForkTree l n r)
+
+proofBST :: ITree t -> ProofBST t
+proofBST EmptyITree        = PE
+proofBST (ForkITree l n r) = case proofGTN r n of
+  PGTN _ _ -> case proofLTN l n of
+    PLTN _ _ -> PF (proofBST l) n (proofBST r)
+
+data LTN :: Tree -> Nat -> * where
+  PLTN :: (LtN t n, IsBST t) =>
+    ITree t -> Natty n -> LTN t n
+
+proofLTN :: ITree t -> Natty n -> LTN t n
+proofLTN EmptyITree         n = PLTN EmptyITree n
+proofLTN t@(ForkITree l n1 r) n = case proofLTN l n of
+  PLTN _ _ -> case proofLTN r n of
+    PLTN _ _ -> case owotoNat n1 n of
+      EE -> undefined -- | Impossible case since we want to prove LtN t n
+      LE -> case proofBST t of
+        PF{} -> PLTN t n
+      GE -> undefined -- | Impossible case since we want to prove LtN t n
+
+data GTN :: Tree -> Nat -> * where
+  PGTN :: (GtN t n, IsBST t) =>
+    ITree t -> Natty n -> GTN t n
+
+proofGTN :: ITree t -> Natty n -> GTN t n
+proofGTN EmptyITree           n = PGTN EmptyITree n
+proofGTN t@(ForkITree l n1 r) n = case proofGTN r n of
+  PGTN _ _ -> case proofGTN l n of
+    PGTN _ _ -> case owotoNat n1 n of
+      EE -> undefined -- | Impossible case since we want to prove GtN t n
+      LE -> undefined -- | Impossible case since we want to prove GtN t n
+      GE -> case proofBST t of
+        PF{} -> PGTN t n
 
 type family Insert (x :: Nat) (t :: Tree) :: Tree where
   Insert x 'EmptyTree         = 'ForkTree 'EmptyTree x 'EmptyTree
@@ -67,7 +109,11 @@ insert x (ForkITree l n r)  = case owotoNat x n of
   GE -> ForkITree l n (insert x r)
 
 insertBST :: Natty x -> BST t -> BST (Insert x t)
-insertBST x (BST t) = BST $ insert x t
+insertBST x (BST t) = let
+  t' = insert x t
+  in case proofBST t' of
+    PE   -> undefined -- | Impossible case since t' = insert x t
+    PF{} -> BST t'
 
 type family Member (x :: Nat) (t :: Tree) :: Bool where
   Member x 'EmptyTree         = 'False
@@ -140,3 +186,10 @@ delete x (ForkITree l n r)  = case owotoNat x n of
           maxL = max l
   LE -> ForkITree (delete x l) n r
   GE -> ForkITree l n (delete x r)
+
+deleteBST :: Natty x -> BST t -> BST (Delete x t)
+deleteBST x (BST t) = let
+  t' = delete x t
+  in case proofBST t' of
+    PE   -> BST EmptyITree
+    PF{} -> BST t'
