191b2f8baa9bff3a5fa2442a2809b8e25ad6548d
Nicolás Rodríguez <18080513+nico-rodriguez@users.noreply.github.com>
2019-08-14 16:41:28 -0300

    Testing BBT2.insertBBT.

--- ITreeNatIncremental/TestITree.hs
+++ ITreeNatIncremental/TestITree.hs
@@ -4,7 +4,7 @@ module ITreeNatIncremental.Test where
 
 import ITreeNatIncremental.ITree
 import ITreeNatIncremental.BST
-import ITreeNatIncremental.BBT
+import ITreeNatIncremental.BBT2
 import Data.Nat
 
 sbn0 :: Natty 'Z
@@ -165,132 +165,278 @@ t11 = deleteBST sbn1 t7
 
 -- | Test Balanced Binary Tree
 
-be :: BBT 'EmptyTree
-be = BBT EmptyITree
+-- be :: BBT 'EmptyTree
+-- be = BBT EmptyITree
+--
+-- -- [4]
+-- bt1 :: BBT ('ForkTree 'EmptyTree ('S ('S ('S ('S 'Z)))) 'EmptyTree)
+-- bt1 = insertBBT sbn4 be
+--
+-- -- [2,4]
+-- bt2 :: BBT
+--         ('ForkTree
+--            ('ForkTree 'EmptyTree ('S ('S 'Z)) 'EmptyTree)
+--            ('S ('S ('S ('S 'Z))))
+--            'EmptyTree)
+-- bt2 = insertBBT sbn2 bt1
+--
+-- -- [2,4,6]
+-- bt3 :: BBT
+--         ('ForkTree
+--            ('ForkTree 'EmptyTree ('S ('S 'Z)) 'EmptyTree)
+--            ('S ('S ('S ('S 'Z))))
+--            ('ForkTree 'EmptyTree ('S ('S ('S ('S ('S ('S 'Z)))))) 'EmptyTree))
+-- bt3 = insertBBT sbn6 bt2
+--
+-- -- [2,3,4,6]
+-- bt4 :: BBT
+--         ('ForkTree
+--            ('ForkTree
+--               'EmptyTree
+--               ('S ('S 'Z))
+--               ('ForkTree 'EmptyTree ('S ('S ('S 'Z))) 'EmptyTree))
+--            ('S ('S ('S ('S 'Z))))
+--            ('ForkTree 'EmptyTree ('S ('S ('S ('S ('S ('S 'Z)))))) 'EmptyTree))
+-- bt4 = insertBBT sbn3 bt3
+--
+-- -- [2,3,4,5,6]
+-- bt5 :: BBT
+--         ('ForkTree
+--            ('ForkTree
+--               'EmptyTree
+--               ('S ('S 'Z))
+--               ('ForkTree 'EmptyTree ('S ('S ('S 'Z))) 'EmptyTree))
+--            ('S ('S ('S ('S 'Z))))
+--            ('ForkTree
+--               ('ForkTree 'EmptyTree ('S ('S ('S ('S ('S 'Z))))) 'EmptyTree)
+--               ('S ('S ('S ('S ('S ('S 'Z))))))
+--               'EmptyTree))
+-- bt5 = insertBBT sbn5 bt4
+--
+-- -- [0,2,3,4,5,6]
+-- bt6 :: BBT
+--         ('ForkTree
+--            ('ForkTree
+--               ('ForkTree 'EmptyTree 'Z 'EmptyTree)
+--               ('S ('S 'Z))
+--               ('ForkTree 'EmptyTree ('S ('S ('S 'Z))) 'EmptyTree))
+--            ('S ('S ('S ('S 'Z))))
+--            ('ForkTree
+--               ('ForkTree 'EmptyTree ('S ('S ('S ('S ('S 'Z))))) 'EmptyTree)
+--               ('S ('S ('S ('S ('S ('S 'Z))))))
+--               'EmptyTree))
+-- bt6 = insertBBT sbn0 bt5
+--
+-- -- [0,2,3,4,5,6,7]
+-- bt7 :: BBT
+--         ('ForkTree
+--            ('ForkTree
+--               ('ForkTree 'EmptyTree 'Z 'EmptyTree)
+--               ('S ('S 'Z))
+--               ('ForkTree 'EmptyTree ('S ('S ('S 'Z))) 'EmptyTree))
+--            ('S ('S ('S ('S 'Z))))
+--            ('ForkTree
+--               ('ForkTree 'EmptyTree ('S ('S ('S ('S ('S 'Z))))) 'EmptyTree)
+--               ('S ('S ('S ('S ('S ('S 'Z))))))
+--               ('ForkTree
+--                  'EmptyTree ('S ('S ('S ('S ('S ('S ('S 'Z))))))) 'EmptyTree)))
+-- bt7 = insertBBT sbn7 bt6
+--
+-- bt8 :: BBT
+--         ('ForkTree
+--            ('ForkTree
+--               ('ForkTree 'EmptyTree 'Z 'EmptyTree)
+--               ('S ('S 'Z))
+--               ('ForkTree 'EmptyTree ('S ('S ('S 'Z))) 'EmptyTree))
+--            ('S ('S ('S ('S 'Z))))
+--            ('ForkTree
+--               ('ForkTree 'EmptyTree ('S ('S ('S ('S ('S 'Z))))) 'EmptyTree)
+--               ('S ('S ('S ('S ('S ('S 'Z))))))
+--               ('ForkTree
+--                  'EmptyTree ('S ('S ('S ('S ('S ('S ('S 'Z))))))) 'EmptyTree)))
+-- bt8 = insertBBT sbn7 bt7
+--
+-- bt9 :: BBT
+--         ('ForkTree
+--            ('ForkTree
+--               ('ForkTree 'EmptyTree 'Z 'EmptyTree)
+--               ('S ('S 'Z))
+--               ('ForkTree 'EmptyTree ('S ('S ('S 'Z))) 'EmptyTree))
+--            ('S ('S ('S ('S 'Z))))
+--            ('ForkTree
+--               ('ForkTree 'EmptyTree ('S ('S ('S ('S ('S 'Z))))) 'EmptyTree)
+--               ('S ('S ('S ('S ('S ('S 'Z))))))
+--               'EmptyTree))
+-- bt9 = deleteBBT sbn7 bt7
+--
+-- bt10 :: BBT
+--          ('ForkTree
+--             ('ForkTree
+--                ('ForkTree 'EmptyTree 'Z 'EmptyTree) ('S ('S 'Z)) 'EmptyTree)
+--             ('S ('S ('S 'Z)))
+--             ('ForkTree
+--                ('ForkTree 'EmptyTree ('S ('S ('S ('S ('S 'Z))))) 'EmptyTree)
+--                ('S ('S ('S ('S ('S ('S 'Z))))))
+--                ('ForkTree
+--                   'EmptyTree ('S ('S ('S ('S ('S ('S ('S 'Z))))))) 'EmptyTree)))
+-- bt10 = deleteBBT sbn4 bt7
+--
+-- bt11 :: BBT
+--          ('ForkTree
+--             ('ForkTree
+--                ('ForkTree 'EmptyTree 'Z 'EmptyTree)
+--                ('S ('S 'Z))
+--                ('ForkTree 'EmptyTree ('S ('S ('S 'Z))) 'EmptyTree))
+--             ('S ('S ('S ('S 'Z))))
+--             ('ForkTree
+--                ('ForkTree 'EmptyTree ('S ('S ('S ('S ('S 'Z))))) 'EmptyTree)
+--                ('S ('S ('S ('S ('S ('S 'Z))))))
+--                ('ForkTree
+--                   'EmptyTree ('S ('S ('S ('S ('S ('S ('S 'Z))))))) 'EmptyTree)))
+-- bt11 = deleteBBT sbn1 bt7
+
+be :: BBT 'EmptyAATree
+be = BBT EmptyIAATree
 
 -- [4]
-bt1 :: BBT ('ForkTree 'EmptyTree ('S ('S ('S ('S 'Z)))) 'EmptyTree)
+bt1 :: BBT ('ForkAATree 'EmptyAATree ('S ('S ('S ('S 'Z)))) 'Z 'EmptyAATree)
 bt1 = insertBBT sbn4 be
 
 -- [2,4]
 bt2 :: BBT
-        ('ForkTree
-           ('ForkTree 'EmptyTree ('S ('S 'Z)) 'EmptyTree)
-           ('S ('S ('S ('S 'Z))))
-           'EmptyTree)
+         ('ForkAATree
+            'EmptyAATree
+            ('S ('S 'Z))
+            'Z
+            ('ForkAATree 'EmptyAATree ('S ('S ('S ('S 'Z)))) 'Z 'EmptyAATree))
 bt2 = insertBBT sbn2 bt1
 
 -- [2,4,6]
 bt3 :: BBT
-        ('ForkTree
-           ('ForkTree 'EmptyTree ('S ('S 'Z)) 'EmptyTree)
-           ('S ('S ('S ('S 'Z))))
-           ('ForkTree 'EmptyTree ('S ('S ('S ('S ('S ('S 'Z)))))) 'EmptyTree))
+        ('ForkAATree
+           ('ForkAATree 'EmptyAATree ('S ('S 'Z)) 'Z 'EmptyAATree)
+           ('S ('S ('S ('S 'Z)))) ('S 'Z)
+           ('ForkAATree 'EmptyAATree ('S ('S ('S ('S ('S ('S 'Z)))))) 'Z 'EmptyAATree))
 bt3 = insertBBT sbn6 bt2
 
 -- [2,3,4,6]
 bt4 :: BBT
-        ('ForkTree
-           ('ForkTree
-              'EmptyTree
-              ('S ('S 'Z))
-              ('ForkTree 'EmptyTree ('S ('S ('S 'Z))) 'EmptyTree))
-           ('S ('S ('S ('S 'Z))))
-           ('ForkTree 'EmptyTree ('S ('S ('S ('S ('S ('S 'Z)))))) 'EmptyTree))
+         ('ForkAATree
+            ('ForkAATree
+               'EmptyAATree
+               ('S ('S 'Z))
+               'Z
+               ('ForkAATree 'EmptyAATree ('S ('S ('S 'Z))) 'Z 'EmptyAATree))
+            ('S ('S ('S ('S 'Z))))
+            ('S 'Z)
+            ('ForkAATree
+               'EmptyAATree ('S ('S ('S ('S ('S ('S 'Z)))))) 'Z 'EmptyAATree))
 bt4 = insertBBT sbn3 bt3
 
 -- [2,3,4,5,6]
 bt5 :: BBT
-        ('ForkTree
-           ('ForkTree
-              'EmptyTree
-              ('S ('S 'Z))
-              ('ForkTree 'EmptyTree ('S ('S ('S 'Z))) 'EmptyTree))
-           ('S ('S ('S ('S 'Z))))
-           ('ForkTree
-              ('ForkTree 'EmptyTree ('S ('S ('S ('S ('S 'Z))))) 'EmptyTree)
-              ('S ('S ('S ('S ('S ('S 'Z))))))
-              'EmptyTree))
+         ('ForkAATree
+            ('ForkAATree
+               'EmptyAATree
+               ('S ('S 'Z))
+               'Z
+               ('ForkAATree 'EmptyAATree ('S ('S ('S 'Z))) 'Z 'EmptyAATree))
+            ('S ('S ('S ('S 'Z))))
+            ('S 'Z)
+            ('ForkAATree
+               'EmptyAATree
+               ('S ('S ('S ('S ('S 'Z)))))
+               'Z
+               ('ForkAATree
+                  'EmptyAATree ('S ('S ('S ('S ('S ('S 'Z)))))) 'Z 'EmptyAATree)))
 bt5 = insertBBT sbn5 bt4
 
 -- [0,2,3,4,5,6]
 bt6 :: BBT
-        ('ForkTree
-           ('ForkTree
-              ('ForkTree 'EmptyTree 'Z 'EmptyTree)
-              ('S ('S 'Z))
-              ('ForkTree 'EmptyTree ('S ('S ('S 'Z))) 'EmptyTree))
-           ('S ('S ('S ('S 'Z))))
-           ('ForkTree
-              ('ForkTree 'EmptyTree ('S ('S ('S ('S ('S 'Z))))) 'EmptyTree)
-              ('S ('S ('S ('S ('S ('S 'Z))))))
-              'EmptyTree))
+         ('ForkAATree
+            ('ForkAATree
+               'EmptyAATree
+               'Z
+               'Z
+               ('ForkAATree 'EmptyAATree ('S ('S 'Z)) 'Z 'EmptyAATree))
+            ('S ('S ('S ('S 'Z))))
+            ('S 'Z)
+            ('ForkAATree
+               'EmptyAATree
+               ('S ('S ('S ('S ('S 'Z)))))
+               'Z
+               ('ForkAATree
+                  'EmptyAATree ('S ('S ('S ('S ('S ('S 'Z)))))) 'Z 'EmptyAATree)))
 bt6 = insertBBT sbn0 bt5
 
 -- [0,2,3,4,5,6,7]
 bt7 :: BBT
-        ('ForkTree
-           ('ForkTree
-              ('ForkTree 'EmptyTree 'Z 'EmptyTree)
-              ('S ('S 'Z))
-              ('ForkTree 'EmptyTree ('S ('S ('S 'Z))) 'EmptyTree))
-           ('S ('S ('S ('S 'Z))))
-           ('ForkTree
-              ('ForkTree 'EmptyTree ('S ('S ('S ('S ('S 'Z))))) 'EmptyTree)
-              ('S ('S ('S ('S ('S ('S 'Z))))))
-              ('ForkTree
-                 'EmptyTree ('S ('S ('S ('S ('S ('S ('S 'Z))))))) 'EmptyTree)))
+         ('ForkAATree
+            ('ForkAATree
+               'EmptyAATree
+               'Z
+               'Z
+               ('ForkAATree 'EmptyAATree ('S ('S 'Z)) 'Z 'EmptyAATree))
+            ('S ('S ('S ('S 'Z))))
+            ('S 'Z)
+            ('ForkAATree
+               ('ForkAATree
+                  'EmptyAATree ('S ('S ('S ('S ('S 'Z))))) 'Z 'EmptyAATree)
+               ('S ('S ('S ('S ('S ('S 'Z))))))
+               ('S 'Z)
+               ('ForkAATree
+                  'EmptyAATree
+                  ('S ('S ('S ('S ('S ('S ('S 'Z)))))))
+                  'Z
+                  'EmptyAATree)))
 bt7 = insertBBT sbn7 bt6
 
 bt8 :: BBT
-        ('ForkTree
-           ('ForkTree
-              ('ForkTree 'EmptyTree 'Z 'EmptyTree)
-              ('S ('S 'Z))
-              ('ForkTree 'EmptyTree ('S ('S ('S 'Z))) 'EmptyTree))
-           ('S ('S ('S ('S 'Z))))
-           ('ForkTree
-              ('ForkTree 'EmptyTree ('S ('S ('S ('S ('S 'Z))))) 'EmptyTree)
-              ('S ('S ('S ('S ('S ('S 'Z))))))
-              ('ForkTree
-                 'EmptyTree ('S ('S ('S ('S ('S ('S ('S 'Z))))))) 'EmptyTree)))
+         ('ForkAATree
+            ('ForkAATree
+               'EmptyAATree
+               'Z
+               'Z
+               ('ForkAATree 'EmptyAATree ('S ('S 'Z)) 'Z 'EmptyAATree))
+            ('S ('S ('S ('S 'Z))))
+            ('S 'Z)
+            ('ForkAATree
+               ('ForkAATree
+                  'EmptyAATree ('S ('S ('S ('S ('S 'Z))))) 'Z 'EmptyAATree)
+               ('S ('S ('S ('S ('S ('S 'Z))))))
+               ('S 'Z)
+               ('ForkAATree
+                  'EmptyAATree
+                  ('S ('S ('S ('S ('S ('S ('S 'Z)))))))
+                  'Z
+                  'EmptyAATree)))
 bt8 = insertBBT sbn7 bt7
 
-bt9 :: BBT
-        ('ForkTree
-           ('ForkTree
-              ('ForkTree 'EmptyTree 'Z 'EmptyTree)
-              ('S ('S 'Z))
-              ('ForkTree 'EmptyTree ('S ('S ('S 'Z))) 'EmptyTree))
-           ('S ('S ('S ('S 'Z))))
-           ('ForkTree
-              ('ForkTree 'EmptyTree ('S ('S ('S ('S ('S 'Z))))) 'EmptyTree)
-              ('S ('S ('S ('S ('S ('S 'Z))))))
-              'EmptyTree))
-bt9 = deleteBBT sbn7 bt7
 
-bt10 :: BBT
-         ('ForkTree
-            ('ForkTree
-               ('ForkTree 'EmptyTree 'Z 'EmptyTree) ('S ('S 'Z)) 'EmptyTree)
-            ('S ('S ('S 'Z)))
-            ('ForkTree
-               ('ForkTree 'EmptyTree ('S ('S ('S ('S ('S 'Z))))) 'EmptyTree)
-               ('S ('S ('S ('S ('S ('S 'Z))))))
-               ('ForkTree
-                  'EmptyTree ('S ('S ('S ('S ('S ('S ('S 'Z))))))) 'EmptyTree)))
-bt10 = deleteBBT sbn4 bt7
+-- bt9 = deleteBBT sbn7 bt7
 
-bt11 :: BBT
-         ('ForkTree
-            ('ForkTree
-               ('ForkTree 'EmptyTree 'Z 'EmptyTree)
-               ('S ('S 'Z))
-               ('ForkTree 'EmptyTree ('S ('S ('S 'Z))) 'EmptyTree))
-            ('S ('S ('S ('S 'Z))))
-            ('ForkTree
-               ('ForkTree 'EmptyTree ('S ('S ('S ('S ('S 'Z))))) 'EmptyTree)
-               ('S ('S ('S ('S ('S ('S 'Z))))))
-               ('ForkTree
-                  'EmptyTree ('S ('S ('S ('S ('S ('S ('S 'Z))))))) 'EmptyTree)))
-bt11 = deleteBBT sbn1 bt7
+-- bt10 :: BBT
+--          ('ForkTree
+--             ('ForkTree
+--                ('ForkTree 'EmptyTree 'Z 'EmptyTree) ('S ('S 'Z)) 'EmptyTree)
+--             ('S ('S ('S 'Z)))
+--             ('ForkTree
+--                ('ForkTree 'EmptyTree ('S ('S ('S ('S ('S 'Z))))) 'EmptyTree)
+--                ('S ('S ('S ('S ('S ('S 'Z))))))
+--                ('ForkTree
+--                   'EmptyTree ('S ('S ('S ('S ('S ('S ('S 'Z))))))) 'EmptyTree)))
+-- bt10 = deleteBBT sbn4 bt7
+--
+-- bt11 :: BBT
+--          ('ForkTree
+--             ('ForkTree
+--                ('ForkTree 'EmptyTree 'Z 'EmptyTree)
+--                ('S ('S 'Z))
+--                ('ForkTree 'EmptyTree ('S ('S ('S 'Z))) 'EmptyTree))
+--             ('S ('S ('S ('S 'Z))))
+--             ('ForkTree
+--                ('ForkTree 'EmptyTree ('S ('S ('S ('S ('S 'Z))))) 'EmptyTree)
+--                ('S ('S ('S ('S ('S ('S 'Z))))))
+--                ('ForkTree
+--                   'EmptyTree ('S ('S ('S ('S ('S ('S ('S 'Z))))))) 'EmptyTree)))
+-- bt11 = deleteBBT sbn1 bt7
