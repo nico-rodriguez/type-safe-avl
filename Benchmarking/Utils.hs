2e20d2f30b98a5b537d22ae15e49b067c327ef55
nico-rodriguez <18080513+nico-rodriguez@users.noreply.github.com>
2019-11-10 00:56:38 -0300

    Fix compiler warnings.

--- Benchmarking/Utils.hs
+++ Benchmarking/Utils.hs
@@ -1,6 +1,9 @@
+{-# LANGUAGE Safe #-}
+
 module Benchmarking.Utils where
 
-import System.Time
+import Prelude (Float, fromInteger, (-), (/), (+))
+import System.Time (ClockTime(TOD))
 
 secDiff :: ClockTime -> ClockTime -> Float
 secDiff (TOD secs1 psecs1) (TOD secs2 psecs2) = fromInteger (psecs2 - psecs1) / 1e12 + fromInteger (secs2 - secs1)
