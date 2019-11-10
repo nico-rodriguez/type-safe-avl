fa90829cfa805eb1e235e2244921d3ca16509f9c
nico-rodriguez <18080513+nico-rodriguez@users.noreply.github.com>
2019-11-09 23:34:16 -0300

    Add file.

---
+++ Benchmarking/Utils.hs
@@ -0,0 +1,6 @@
+module Benchmarking.Utils where
+
+import System.Time
+
+secDiff :: ClockTime -> ClockTime -> Float
+secDiff (TOD secs1 psecs1) (TOD secs2 psecs2) = fromInteger (psecs2 - psecs1) / 1e12 + fromInteger (secs2 - secs1)
