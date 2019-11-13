64671982d6a50802415747c53b2a586f6dc39466
nico-rodriguez <18080513+nico-rodriguez@users.noreply.github.com>
2019-11-13 09:41:30 -0300

    Change benchmarking values.

--- Benchmarking/Extern/Benchmark.hs
+++ Benchmarking/Extern/Benchmark.hs
@@ -11,69 +11,69 @@ import           Prelude                        (Bool (False), IO, putStrLn,
 import           System.Time                    (getClockTime)
 
 
-t50 :: AVL (InsertN 50 'False 'EmptyTree)
-t50 = insertN (Proxy::Proxy 50) (Proxy::Proxy 'False) (AVL EmptyITree)
-t100 :: AVL (InsertN 100 'False 'EmptyTree)
-t100 = insertN (Proxy::Proxy 100) (Proxy::Proxy 'False) (AVL EmptyITree)
-t150 :: AVL (InsertN 150 'False 'EmptyTree)
-t150 = insertN (Proxy::Proxy 150) (Proxy::Proxy 'False) (AVL EmptyITree)
-t200 :: AVL (InsertN 200 'False 'EmptyTree)
-t200 = insertN (Proxy::Proxy 200) (Proxy::Proxy 'False) (AVL EmptyITree)
+t500 :: AVL (InsertN 500 'False 'EmptyTree)
+t500 = insertN (Proxy::Proxy 500) (Proxy::Proxy 'False) (AVL EmptyITree)
+t1000 :: AVL (InsertN 1000 'False 'EmptyTree)
+t1000 = insertN (Proxy::Proxy 1000) (Proxy::Proxy 'False) (AVL EmptyITree)
+t1500 :: AVL (InsertN 1500 'False 'EmptyTree)
+t1500 = insertN (Proxy::Proxy 1500) (Proxy::Proxy 'False) (AVL EmptyITree)
+t2000 :: AVL (InsertN 2000 'False 'EmptyTree)
+t2000 = insertN (Proxy::Proxy 2000) (Proxy::Proxy 'False) (AVL EmptyITree)
 e1 :: AVL 'EmptyTree
-e1 = deleteN (Proxy::Proxy 50) t50
+e1 = deleteN (Proxy::Proxy 500) t500
 e2 :: AVL 'EmptyTree
-e2 = deleteN (Proxy::Proxy 100) t100
+e2 = deleteN (Proxy::Proxy 1000) t1000
 e3 :: AVL 'EmptyTree
-e3 = deleteN (Proxy::Proxy 150) t150
+e3 = deleteN (Proxy::Proxy 1500) t1500
 e4 :: AVL 'EmptyTree
-e4 = deleteN (Proxy::Proxy 200) t200
+e4 = deleteN (Proxy::Proxy 2000) t2000
 v1 :: Char
-v1 = lookupAVL (Proxy::Proxy 50) t50
+v1 = lookupAVL (Proxy::Proxy 500) t500
 v2 :: Char
-v2 = lookupAVL (Proxy::Proxy 100) t100
+v2 = lookupAVL (Proxy::Proxy 1000) t1000
 v3 :: Char
-v3 = lookupAVL (Proxy::Proxy 150) t150
+v3 = lookupAVL (Proxy::Proxy 1500) t1500
 v4 :: Char
-v4 = lookupAVL (Proxy::Proxy 200) t200
+v4 = lookupAVL (Proxy::Proxy 2000) t2000
 
 main :: IO ()
 main = do t0 <- getClockTime
           -- Insert
-          seq t50 (return ())
+          seq t500 (return ())
           t1 <- getClockTime
-          putStrLn ("Insert time (N=50): " ++ show (secDiff t0 t1) ++ " seconds")
-          seq t100 (return ())
+          putStrLn ("Insert time (N=500): " ++ show (secDiff t0 t1) ++ " seconds")
+          seq t1000 (return ())
           t1 <- getClockTime
-          putStrLn ("Insert time (N=100): " ++ show (secDiff t0 t1) ++ " seconds")
-          seq t150 (return ())
+          putStrLn ("Insert time (N=1000): " ++ show (secDiff t0 t1) ++ " seconds")
+          seq t1500 (return ())
           t1 <- getClockTime
-          putStrLn ("Insert time (N=150): " ++ show (secDiff t0 t1) ++ " seconds")
-          seq t200 (return ())
+          putStrLn ("Insert time (N=1500): " ++ show (secDiff t0 t1) ++ " seconds")
+          seq t2000 (return ())
           t1 <- getClockTime
-          putStrLn ("Insert time (N=200): " ++ show (secDiff t0 t1) ++ " seconds")
+          putStrLn ("Insert time (N=2000): " ++ show (secDiff t0 t1) ++ " seconds")
           -- Delete
           seq e1 (return ())
           t1 <- getClockTime
-          putStrLn ("Delete time (N=50): " ++ show (secDiff t0 t1) ++ " seconds")
+          putStrLn ("Delete time (N=500): " ++ show (secDiff t0 t1) ++ " seconds")
           seq e2 (return ())
           t1 <- getClockTime
-          putStrLn ("Delete time (N=100): " ++ show (secDiff t0 t1) ++ " seconds")
+          putStrLn ("Delete time (N=1000): " ++ show (secDiff t0 t1) ++ " seconds")
           seq e3 (return ())
           t1 <- getClockTime
-          putStrLn ("Delete time (N=150): " ++ show (secDiff t0 t1) ++ " seconds")
+          putStrLn ("Delete time (N=1500): " ++ show (secDiff t0 t1) ++ " seconds")
           seq e4 (return ())
           t1 <- getClockTime
-          putStrLn ("Delete time (N=200): " ++ show (secDiff t0 t1) ++ " seconds")
+          putStrLn ("Delete time (N=2000): " ++ show (secDiff t0 t1) ++ " seconds")
           -- Lookup
           seq v1 (return ())
           t1 <- getClockTime
-          putStrLn ("Lookup time (N=50): " ++ show (secDiff t0 t1) ++ " seconds")
+          putStrLn ("Lookup time (N=500): " ++ show (secDiff t0 t1) ++ " seconds")
           seq v2 (return ())
           t1 <- getClockTime
-          putStrLn ("Lookup time (N=100): " ++ show (secDiff t0 t1) ++ " seconds")
+          putStrLn ("Lookup time (N=1000): " ++ show (secDiff t0 t1) ++ " seconds")
           seq v3 (return ())
           t1 <- getClockTime
-          putStrLn ("Lookup time (N=150): " ++ show (secDiff t0 t1) ++ " seconds")
+          putStrLn ("Lookup time (N=1500): " ++ show (secDiff t0 t1) ++ " seconds")
           seq v4 (return ())
           t1 <- getClockTime
-          putStrLn ("Lookup time (N=200): " ++ show (secDiff t0 t1) ++ " seconds")
+          putStrLn ("Lookup time (N=2000): " ++ show (secDiff t0 t1) ++ " seconds")
