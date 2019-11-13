d7f4ebdd1acaf4afe0ba6080760e85e7c73dc56a
nico-rodriguez <18080513+nico-rodriguez@users.noreply.github.com>
2019-11-13 12:23:33 -0300

    Change benchmarking values.

--- Benchmarking/FullExtern/Benchmark.hs
+++ Benchmarking/FullExtern/Benchmark.hs
@@ -10,69 +10,69 @@ import           Prelude                        (Bool (False), IO, putStrLn,
 import           Data.Time.Clock                (diffUTCTime, getCurrentTime)
 
 
-t500 :: AVL (InsertN 500 'False 'EmptyTree)
-t500 = insertN (Proxy::Proxy 500) (Proxy::Proxy 'False)
-t1000 :: AVL (InsertN 1000 'False 'EmptyTree)
-t1000 = insertN (Proxy::Proxy 1000) (Proxy::Proxy 'False)
-t1500 :: AVL (InsertN 1500 'False 'EmptyTree)
-t1500 = insertN (Proxy::Proxy 1500) (Proxy::Proxy 'False)
-t2000 :: AVL (InsertN 2000 'False 'EmptyTree)
-t2000 = insertN (Proxy::Proxy 2000) (Proxy::Proxy 'False)
+t100 :: AVL (InsertN 100 'False 'EmptyTree)
+t100 = insertN (Proxy::Proxy 100) (Proxy::Proxy 'False)
+t200 :: AVL (InsertN 200 'False 'EmptyTree)
+t200 = insertN (Proxy::Proxy 200) (Proxy::Proxy 'False)
+t300 :: AVL (InsertN 300 'False 'EmptyTree)
+t300 = insertN (Proxy::Proxy 300) (Proxy::Proxy 'False)
+t400 :: AVL (InsertN 400 'False 'EmptyTree)
+t400 = insertN (Proxy::Proxy 400) (Proxy::Proxy 'False)
 e1 :: AVL 'EmptyTree
-e1 = deleteN (Proxy::Proxy 500) t500
+e1 = deleteN (Proxy::Proxy 100) t100
 e2 :: AVL 'EmptyTree
-e2 = deleteN (Proxy::Proxy 1000) t1000
+e2 = deleteN (Proxy::Proxy 200) t200
 e3 :: AVL 'EmptyTree
-e3 = deleteN (Proxy::Proxy 1500) t1500
+e3 = deleteN (Proxy::Proxy 300) t300
 e4 :: AVL 'EmptyTree
-e4 = deleteN (Proxy::Proxy 2000) t2000
+e4 = deleteN (Proxy::Proxy 400) t400
 v1 :: Char
-v1 = lookupAVL (Proxy::Proxy 500) t500
+v1 = lookupAVL (Proxy::Proxy 100) t100
 v2 :: Char
-v2 = lookupAVL (Proxy::Proxy 1000) t1000
+v2 = lookupAVL (Proxy::Proxy 200) t200
 v3 :: Char
-v3 = lookupAVL (Proxy::Proxy 1500) t1500
+v3 = lookupAVL (Proxy::Proxy 300) t300
 v4 :: Char
-v4 = lookupAVL (Proxy::Proxy 2000) t2000
+v4 = lookupAVL (Proxy::Proxy 400) t400
 
 main :: IO ()
 main = do t0 <- getCurrentTime
           -- Insert
-          seq t500 (return ())
+          seq t100 (return ())
           t1 <- getCurrentTime
-          putStrLn ("Insert time (N=500): " ++ show (diffUTCTime t0 t1) ++ " seconds")
-          seq t1000 (return ())
+          putStrLn ("Insert time (N=100): " ++ show (diffUTCTime t0 t1) ++ " seconds")
+          seq t200 (return ())
           t1 <- getCurrentTime
-          putStrLn ("Insert time (N=1000): " ++ show (diffUTCTime t0 t1) ++ " seconds")
-          seq t1500 (return ())
+          putStrLn ("Insert time (N=200): " ++ show (diffUTCTime t0 t1) ++ " seconds")
+          seq t300 (return ())
           t1 <- getCurrentTime
-          putStrLn ("Insert time (N=1500): " ++ show (diffUTCTime t0 t1) ++ " seconds")
-          seq t2000 (return ())
+          putStrLn ("Insert time (N=300): " ++ show (diffUTCTime t0 t1) ++ " seconds")
+          seq t400 (return ())
           t1 <- getCurrentTime
-          putStrLn ("Insert time (N=2000): " ++ show (diffUTCTime t0 t1) ++ " seconds")
+          putStrLn ("Insert time (N=400): " ++ show (diffUTCTime t0 t1) ++ " seconds")
           -- Delete
           seq e1 (return ())
           t1 <- getCurrentTime
-          putStrLn ("Delete time (N=500): " ++ show (diffUTCTime t0 t1) ++ " seconds")
+          putStrLn ("Delete time (N=100): " ++ show (diffUTCTime t0 t1) ++ " seconds")
           seq e2 (return ())
           t1 <- getCurrentTime
-          putStrLn ("Delete time (N=1000): " ++ show (diffUTCTime t0 t1) ++ " seconds")
+          putStrLn ("Delete time (N=200): " ++ show (diffUTCTime t0 t1) ++ " seconds")
           seq e3 (return ())
           t1 <- getCurrentTime
-          putStrLn ("Delete time (N=1500): " ++ show (diffUTCTime t0 t1) ++ " seconds")
+          putStrLn ("Delete time (N=300): " ++ show (diffUTCTime t0 t1) ++ " seconds")
           seq e4 (return ())
           t1 <- getCurrentTime
-          putStrLn ("Delete time (N=2000): " ++ show (diffUTCTime t0 t1) ++ " seconds")
+          putStrLn ("Delete time (N=400): " ++ show (diffUTCTime t0 t1) ++ " seconds")
           -- Lookup
           seq v1 (return ())
           t1 <- getCurrentTime
-          putStrLn ("Lookup time (N=500): " ++ show (diffUTCTime t0 t1) ++ " seconds")
+          putStrLn ("Lookup time (N=100): " ++ show (diffUTCTime t0 t1) ++ " seconds")
           seq v2 (return ())
           t1 <- getCurrentTime
-          putStrLn ("Lookup time (N=1000): " ++ show (diffUTCTime t0 t1) ++ " seconds")
+          putStrLn ("Lookup time (N=200): " ++ show (diffUTCTime t0 t1) ++ " seconds")
           seq v3 (return ())
           t1 <- getCurrentTime
-          putStrLn ("Lookup time (N=1500): " ++ show (diffUTCTime t0 t1) ++ " seconds")
+          putStrLn ("Lookup time (N=300): " ++ show (diffUTCTime t0 t1) ++ " seconds")
           seq v4 (return ())
           t1 <- getCurrentTime
-          putStrLn ("Lookup time (N=2000): " ++ show (diffUTCTime t0 t1) ++ " seconds")
+          putStrLn ("Lookup time (N=400): " ++ show (diffUTCTime t0 t1) ++ " seconds")
