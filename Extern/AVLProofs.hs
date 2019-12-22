0bf109ec8f61606e1b245f28c27d5d72d9ff6183
Nicolás Rodríguez <18080513+nico-rodriguez@users.noreply.github.com>
2019-12-21 21:05:26 -0300

    Remove pragmas implied by another extension

--- Extern/AVLProofs.hs
+++ Extern/AVLProofs.hs
@@ -1,5 +1,4 @@
 {-# LANGUAGE DataKinds             #-}
-{-# LANGUAGE ExplicitNamespaces    #-}
 {-# LANGUAGE FlexibleContexts      #-}
 {-# LANGUAGE FlexibleInstances     #-}
 {-# LANGUAGE GADTs                 #-}
