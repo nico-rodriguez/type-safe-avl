534ea4fa6af836da0d0afdd8abca3aff35f084e2
Nicolás Rodríguez <18080513+nico-rodriguez@users.noreply.github.com>
2020-06-10 22:28:21 -0300

    Add proofs for Nats

---
+++ Data/NatProofs.hs
@@ -0,0 +1,25 @@
+{-# LANGUAGE DataKinds            #-}
+{-# LANGUAGE ExplicitNamespaces   #-}
+{-# LANGUAGE TypeFamilies         #-}
+{-# LANGUAGE TypeOperators        #-}
+
+module Data.NatProofs (
+    proofLTGT, proofGTLT
+) where
+
+import           Data.Proxy      (Proxy)
+import           Data.Type.Equality ((:~:)(Refl))
+import           GHC.TypeNats    (type (+), type (-), type (<=?), Nat, CmpNat)
+import           Prelude         (Bool (False, True), Ordering(LT,GT), undefined)
+import           Unsafe.Coerce   (unsafeCoerce)
+
+
+-- | If n is lesser than m, then m is greater than n
+-- | forall (n,m::Nat), CmpNat n m ~ 'LT -> CmpNat m n ~ 'GT
+proofLTGT :: Proxy n -> Proxy m -> CmpNat n m :~: 'LT -> CmpNat m n :~: 'GT
+proofLTGT _ _ _ = unsafeCoerce Refl
+
+-- | If n is greater than m, then m is lesser than n
+-- | forall (n,m::Nat), CmpNat n m ~ 'GT -> CmpNat m n ~ 'LT
+proofGTLT :: Proxy n -> Proxy m -> CmpNat n m :~: 'GT -> CmpNat m n :~: 'LT
+proofGTLT _ _ _ = unsafeCoerce Refl
