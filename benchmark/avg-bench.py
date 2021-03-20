4b1fc4a6e8739ac837a7309090ece341b8888ba1
Nicolás Rodríguez <18080513+nico-rodriguez@users.noreply.github.com>
2021-03-20 15:51:54 -0300

    Format compilation_time_benchmark output

--- benchmark/avg-bench.py
+++ benchmark/avg-bench.py
@@ -181,7 +181,7 @@ def compilation_time_benchmark(bench_name, operation, bench_id, n, debug):
                  shell=True, capture_output=True, text=True)
     if (debug):
         print("***compilation_time_benchmark***", result, sep="\n")
-    return float(result.stdout)
+    return '{:.2f}'.format(float(result.stdout))
 
 
 def execute_run_time_benchmarks(bench_name, n, save_to_file, debug):
